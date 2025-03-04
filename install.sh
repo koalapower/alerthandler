15d110f350cabb59bcad444ff36e8396422fb2e7
KoalaPower <dmitry3757@gmail.com>
2025-03-04 15:46:03 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,22 +5,20 @@ mkdir /opt/alerthandler/db/
 mkdir /opt/alerthandler/certs/
 mkdir /opt/alertmanager/
 
-tar -xvf $1 --one-top-level=/opt/alerthandler/
-tar -xvf $2 --one-top-level=/opt/alertmanager/ --strip-components=1
+tar -xvf $1 --one-top-level=/opt/alertmanager/ --strip-components=1
+
+cp src/* /opt/alerthadler/
+PYTHON="$(which python3)"
+sed -i "s|python3|$PYTHON|g" templates/alerthandler/alerthandler.service
+cp templates/alerthandler/config.yml /opt/alerthandler/
+cp templates/alerthandler/alerthandler.service /etc/systemd/system/
+
+cp templates/alertmanager/alertmanager.yml /opt/alertmanager/
+cp templates/alertmanager/email.tmpl /opt/alertmanager/
+cp templates/alertmanager/alertmanager.service /etc/systemd/system/
 
 useradd -Mrs /usr/bin/false alerthandler
 useradd -Mrs /usr/bin/false alertmanager
-
-
-PYTHON="$(which python3)"
-sed -i "s|python3|$PYTHON|g" /opt/alerthandler/templates/alerthandler/alerthandler.service
-
-cp /opt/alerthandler/templates/alerthandler/alerthandler.service /etc/systemd/system/
-cp /opt/alerthandler/templates/alerthandler/config.yml /opt/alerthandler/
-
-cp /opt/alerthandler/templates/alertmanager/alertmanager.yml /opt/alertmanager/
-cp /opt/alerthandler/templates/alertmanager/email.tmpl /opt/alertmanager/
-cp /opt/alerthandler/templates/alertmanager/alertmanager.service /etc/systemd/system/
 
 chown -R alertmanager:alertmanager /opt/alertmanager
 chown -R alerthandler:alerthandler /opt/alerthandler
